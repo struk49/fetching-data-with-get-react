6431742601571ed80de5f0ccd7867e207f968e03
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-06-25 20:28:23 +0000

    how to handle api responses object and arrays

--- my-app/src/components/HTTPResponse.js
+++ my-app/src/components/HTTPResponse.js
@@ -1,31 +1,57 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+import axios from 'axios';
 
-export class HTTPResponse extends Component {
+export class HTTPRequests extends Component {
     constructor(props) {
-      super(props)
+        super(props)
     
-      this.state = {
-            post: []
-      }
+        this.state = {
+             posts: [],
+             error: null
+        }
     }
     componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('https://jsonplaceholder.typicode.com/posts/1')
         .then(response => {
             console.log(response);
             this.setState({
-                posts: response.data
+                posts: Array.isArray(response.data)
+                ? response.data
+                : [response.data]
+            })
         })
+        .catch(error => {
+            this.setState({
+                error: error.message
+            })
         })
     }
-  render() {
-    return (
-      <div>
-        <h2>POSTS</h2>
-        {JSON.stringify(this.state.posts)}
-      </div>
-    )
-  }
+    render() {
+        const posts = this.state.posts;
+        return (
+            <div>
+                <h2>Posts:</h2>
+                {
+                    posts.length ? (
+                        posts.map(post => (
+                            <div key={post.id}>
+                                <h3 class="post-title">{post.id}. {post.title}</h3>
+                                <br />
+                                <h4>By User ID {post.userId}</h4>
+                                <br />
+                                <p>{post.body}</p>
+                                <hr />
+                            </div>
+                        ))
+                    ) : (
+                        this.state.error
+                        ? <p>{this.state.error}</p>
+                        : <h4>Loading posts ...</h4>
+                    )
+                }
+            </div>
+        )
+    }
 }
 
-export default HTTPResponse
+export default HTTPRequests
